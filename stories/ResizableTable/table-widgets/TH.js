45ae351b1c86e13d54be55b1fbf81e408776214c
aimun nahar <aimunnaharr@gmail.com>
2023-12-14 12:43:10 +0600

    updated

--- stories/ResizableTable/table-widgets/TH.js
+++ stories/ResizableTable/table-widgets/TH.js
@@ -1,4 +1,4 @@
-const { randomIdGenerator } = require( "../../../utility/utils" );
+const { randomIdGenerator } = require("../../../utility/utils");
 
 
 const ChevronIcon = () => {
@@ -9,7 +9,7 @@ const ChevronIcon = () => {
         </svg>
     </>;
 };
-const PinIcon = ( { column } ) => {
+const PinIcon = ({ column }) => {
     return <>
         <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill={column.isFixed ? "green" : 'gray'} viewBox="0 0 16 16">
             <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354zm1.58 1.408-.002-.001.002.001zm-.002-.001.002.001A.5.5 0 0 1 6 2v5a.5.5 0 0 1-.276.447h-.002l-.012.007-.054.03a4.922 4.922 0 0 0-.827.58c-.318.278-.585.596-.725.936h7.792c-.14-.34-.407-.658-.725-.936a4.915 4.915 0 0 0-.881-.61l-.012-.006h-.002A.5.5 0 0 1 10 7V2a.5.5 0 0 1 .295-.458 1.775 1.775 0 0 0 .351-.271c.08-.08.155-.17.214-.271H5.14c.06.1.133.191.214.271a1.78 1.78 0 0 0 .37.282z" />
@@ -17,29 +17,29 @@ const PinIcon = ( { column } ) => {
     </>;
 };
 
-const TH = ( props ) => {
+const TH = (props) => {
     const { column, index: i, getLeftDistanceOfFixedColumn, expandableRows, handleSort, setAllColumns, allColumns, tableId, columnCache, selectableRows } = props;
     const theadId = randomIdGenerator();
     const makeFixColumn = () => {
-        const updatedData = allColumns.map( c => {
-            if ( c.id === column.id ) {
-                if ( c?.isFixed ) {
+        const updatedData = allColumns.map(c => {
+            if (c.id === column.id) {
+                if (c?.isFixed) {
                     return { ...c, isFixed: false };
                 }
                 return { ...c, isFixed: true };
             }
             return c;
-        } );
-        setAllColumns( updatedData );
-        if ( columnCache ) {
-            localStorage.setItem( tableId, JSON.stringify( updatedData ) );
+        });
+        setAllColumns(updatedData);
+        if (columnCache) {
+            localStorage.setItem(tableId, JSON.stringify(updatedData));
         }
-        const thead = document.getElementById( theadId );
-        const divNodes = thead.querySelectorAll( 'div' );
+        const thead = document.getElementById(theadId);
+        const divNodes = thead.querySelectorAll('div');
 
-        divNodes.forEach( elm => {
-            thead.removeChild( elm );
-        } );
+        divNodes.forEach(elm => {
+            thead.removeChild(elm);
+        });
     };
 
     return <th
@@ -50,7 +50,7 @@ const TH = ( props ) => {
             width: column.width ? column.width : '100px',
             minWidth: column.minWidth ? column.minWidth : '80px',
             textAlign: column.type === 'action' ? 'center' : column.type === 'number' ? 'center' : 'left',
-            left: column.isFixed && getLeftDistanceOfFixedColumn( expandableRows && selectableRows ? i + 2 : expandableRows || selectableRows ? i + 1 : i )
+            left: column.isFixed && getLeftDistanceOfFixedColumn(expandableRows && selectableRows ? i + 2 : expandableRows || selectableRows ? i + 1 : i)
         }}
         id={theadId}
     >
@@ -59,7 +59,7 @@ const TH = ( props ) => {
             width: '100%'
         }}>
             <span
-                onClick={() => handleSort( column )}
+                onClick={() => handleSort(column)}
                 style={{
                     display: 'flex',
                     alignItems: 'center',
@@ -78,7 +78,7 @@ const TH = ( props ) => {
                 style={
                     {
                         color: column.isFixed ? 'green' : 'gray',
-                        display: column.isFixed ? 'block' : ''
+                        display: column.isFixed ? 'block' : '',
                     }
                 }
             >
